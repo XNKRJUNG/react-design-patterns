fe65c79b7ec6bbb811e6ad9dd936fad2b8f12a53
cyrus-xnkrjungg <shresthacyrus@outlook.com>
2024-05-02 18:32:05 -0400

    Split-screen component improvements

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import { SplitScreen } from "./SplitScreen"
+
+const LeftHandComponent = props => {
+  const { name } = props
+  return <h1 style={{ backgroundColor: "green" }}>{name}</h1>
+}
+const RightHandComponent = ({ name }) => {
+  return <p style={{ backgroundColor: "red" }}>{name}</p>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // <SplitScreen left={LeftHandComponent} right={RightHandComponent} leftWeight={1} rightWeight={5} />
+    <SplitScreen leftWeight={1} rightWeight={5}>
+      <LeftHandComponent name="Welcome" />
+      <RightHandComponent name="Right Component" />
+    </SplitScreen>
+  )
 }
 
-export default App;
+export default App
